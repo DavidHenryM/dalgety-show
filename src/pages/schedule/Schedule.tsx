8a348cb7d7c6ff48c20d2b18369226523600c339
David Moorhouse <david@locusinnovations.com.au>
2025-12-24 12:32:47 +1100

    add pdf viewer

--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -1,24 +1,63 @@
-import { Paper } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
+import horseSchedule2025 from "../../assets/documents/81st Annual Dalgety Show 2025 Horse Schedule.pdf"
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+import { useState } from "react";
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { Background } from "../../components/Background";
+import horseJump2 from "../../assets/images/gallery/Horse_Jump_2.jpg"
+
+
+
 
 export default function Schedule(props: {sideBarWidth: number}){
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPageNumber(value);
+  };
+
+
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+  ).toString();
+
+    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+  
+
+
+
   return (
-    <Paper sx={{ml: `${props.sideBarWidth}px`}}>
-    <section className="container mx-auto px-6 py-12">
-      <h2 className="text-3xl font-bold mb-6">Schedules & Downloads</h2>
-      <ul className="space-y-3">
-        <li>
-          <a className="text-blue-600 hover:underline" href="#">
-            Main Show Schedule (PDF)
-          </a>
-        </li>
-        <li>
-          <a className="text-blue-600 hover:underline" href="#">
-            Horse Schedule (PDF)
-          </a>
-        </li>
-      </ul>
-    </section>
-    </Paper>
+    <>
+    <Background image={horseJump2}/>
+    <Grid container spacing={2} padding={2} sx={{ml: `${props.sideBarWidth}px`, mt: "25px", justifyContent: "center"}}>
+      <Grid size={5} alignItems={"center"}>
+        <Paper sx={{ml: `${props.sideBarWidth}px`, justifyItems: "center"}}>
+          <Document file={horseSchedule2025} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </Paper>
+      </Grid>
+      <Grid>
+                <Stack spacing={2}>
+        <Typography>Page: {pageNumber}</Typography>
+        <Pagination count={numPages} page={pageNumber} onChange={handlePageChange} />
+      </Stack>
+      </Grid>
+</Grid>
+    </>
   );
 };
+
+
+
+
+
 
