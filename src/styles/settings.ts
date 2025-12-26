5ae0c466ea54d1d2aad00cda1e09e9ef71316a54
David Moorhouse <david@locusinnovations.com.au>
2025-12-26 21:33:36 +1100

    wth of small drawer when open

--- src/styles/settings.ts
+++ src/styles/settings.ts
@@ -1,1 +1,2 @@
-export const drawerWidth = {sm: "0px", md: "50px", lg: "240px"}
+export const drawerWidth = {sm: "0px", smDrawerOpen: "240px", md: "50px", lg: "240px"}
+export type DrawerWidth = typeof drawerWidth
