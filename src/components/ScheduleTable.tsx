6b67a05c72cb41bf859fb2dd0a24e74836eac614
David Moorhouse <david@locusinnovations.com.au>
2025-12-26 21:38:56 +1100

    fixed name typo

--- src/components/ScheduleTable.tsx
+++ src/components/ScheduleTable.tsx
@@ -1,6 +1,6 @@
 import { DataGrid } from '@mui/x-data-grid';
 import { Box } from "@mui/material";
-import { columns, rows } from "../data/shedule";
+import { columns, rows } from "../data/schedule";
 
 export function ScheduleTable(props: {sideBarWidth: number}){
   <Box sx={{ height: 400, width: "fit-content", ml: `${props.sideBarWidth}px`, justifySelf: "center" }}>
