fa0e4cb2caa4738e22e29e025aab95fc47d1e0ff
David Moorhouse <david@locusinnovations.com.au>
2025-12-28 21:01:42 +1100

    loading icon

---
+++ app/Loading.tsx
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHorse } from "@fortawesome/free-solid-svg-icons"
+
+
+
+export default function Loading(){
+  <FontAwesomeIcon icon={faHorse} bounce />
+}
