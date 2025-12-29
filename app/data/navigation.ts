f20bb04b1e12cf468ccae09bd656edae272992a4
David Moorhouse <david@locusinnovations.com.au>
2025-12-29 20:33:04 +1100

    admin nav

--- app/data/navigation.ts
+++ app/data/navigation.ts
@@ -7,6 +7,7 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import CollectionsIcon from '@mui/icons-material/Collections';
 import ContactMailIcon from '@mui/icons-material/ContactMail';
 import CardMembershipIcon from '@mui/icons-material/CardMembership';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import Home from "../home/page";
 import Events from "../events/page";
 import Schedule from "../schedule/page";
@@ -15,6 +16,7 @@ import Gallery from "../gallery/page";
 import About from "../about/page";
 import Contact from "../contact/page";
 import Sponsors from "../sponsors/page";
+import Admin from "../admin/page";
 
 export const navigation: NavItem[] = [
   { label: "HOME", path: "../home/", Icon: HomeIcon, Content: Home},
@@ -25,4 +27,8 @@ export const navigation: NavItem[] = [
   { label: "GALLERY", path: "../gallery/", Icon: CollectionsIcon , Content: Gallery},
   { label: "CONTACT", path: "../contact/",  Icon: ContactMailIcon, Content: Contact},
   { label: "ABOUT", path: "../about/", Icon: InfoIcon, Content: About},
+];
+
+export const adminNavigation: NavItem[] = [
+  { label: "ADMIN", path: "../admin/", Icon: AdminPanelSettingsIcon, Content: Admin},
 ];
