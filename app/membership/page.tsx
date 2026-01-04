cabb8244976a060cf1f986f43af18027c7761125
David Moorhouse <david@locusinnovations.com.au>
2026-01-04 13:19:20 +1100

    new page

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -1,27 +1,18 @@
-import { Paper } from "@mui/material";
-import { drawerWidth, footerHeight } from "../settings";
+'use client'
+
+import { Button, Link, Typography } from "@mui/material";
+import Content from "../components/Content"
 
 export default function Membership(){
   return (
-    <Paper 
-      sx={{
-        ml: {
-          sm: drawerWidth.sm,
-          md: drawerWidth.md,
-          lg: drawerWidth.lg
-        },
-        mb: footerHeight
-      }}
-    >
-    <section className="container mx-auto px-6 py-12">
-      <h2 className="text-3xl font-bold mb-6">Membership</h2>
-      <p className="text-lg text-gray-700">
-        Become a member of the Dalgety Show Society and support the continuation
-        of this iconic rural event.
-      </p>
-    </section>
-    </Paper>
-  );
-};
-
-
+    <Content backgroundImageIndex={0} >
+      <Typography variant="h4" justifySelf={"center"} color="primary">Become a Member</Typography>
+      <Typography>The Dalgety Show Society Inc. is a devoted group of people who feel strongly about the future of rural Australia and understand the importance of local Shows.</Typography>
+      <Typography> Every year the committee organises for the Dalgety Show, new and exciting events to keep the show fresh and appealing to not only the residents of the Snowy River Shire but visitors to the Snowy Mountains and the Monaro.</Typography>
+      <Typography> The Dalgety Show Society Inc. welcomes new members. Membership costs just $10 per person or $20 per family.</Typography>
+      <Link href="/membership/apply">
+        <Button variant="contained">Apply</Button>
+      </Link>
+    </Content>
+  )
+}
