1401515e6236c40a332e60b7c73b9df2737147e3
David Moorhouse <david@locusinnovations.com.au>
2026-01-04 13:15:06 +1100

    moved standard content setup into component

---
+++ app/components/Content.tsx
@@ -0,0 +1,41 @@
+import { Background } from "@/app/components/Background";
+import { backgroundImages } from "@/app/images/backgrounds";
+import { drawerWidth, footerHeight } from "@/app/settings";
+import { Grid, Paper } from "@mui/material";
+import { ReactNode } from 'react';
+
+interface ContentProps {
+  children: ReactNode;
+  backgroundImageIndex: number
+}
+
+export default function Content({children, backgroundImageIndex}: ContentProps){
+  return (
+    <>
+      <Background image={backgroundImages[backgroundImageIndex]} />
+      <Grid 
+        container 
+        spacing={2} 
+        sx={{
+          p: {
+            sm: 1, 
+            md: 2, 
+            lg: 10
+          }, 
+          ml: {
+            sm: drawerWidth.sm,
+            md: drawerWidth.md,
+            lg: drawerWidth.lg
+          },
+          mb: footerHeight
+        }}
+      >
+        <Grid size={{md:12, lg:6}} spacing={2} p={2} sx={{justifyContent:"center"}}>
+          <Paper sx={{p:2}}>
+            {children}
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
+)
+}
