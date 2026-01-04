cf421196beef8fd6118113bddb7e3d305252cf17
David Moorhouse <david@locusinnovations.com.au>
2026-01-04 13:14:11 +1100

    auto redirect to home from landing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home', // The page to redirect to
+        permanent: true, // true = 308 permanent redirect, false = 307 temporary
+      },
+    ]
+  },
+}
+
 
 export default nextConfig;
