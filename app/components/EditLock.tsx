8406884e5f13307f906b4f8f652943483ab0935d
David Moorhouse <David.Moorhouse@thecreativeelement.com.au>
2026-01-16 22:10:32 +1100

    break out functions

---
+++ app/components/EditLock.tsx
@@ -0,0 +1,56 @@
+import { Dialog, DialogContent, DialogTitle, Fab, Tooltip, DialogContentText, DialogActions, Button } from "@mui/material";
+import { Dispatch, SetStateAction, useState } from "react";
+import RestrictedAccess from "@components/Restricted";
+import LockOutlineIcon from '@mui/icons-material/LockOutline'
+import LockOpenIcon from '@mui/icons-material/LockOpen'
+import { TransitionUp } from "@components/Tansitions";
+
+export default function EditLock(props: {
+  locked: boolean, 
+  setLocked: Dispatch<SetStateAction<boolean>>,
+  userFirstName?: string | null
+})
+{
+  const [openDialog, setOpenDialog] = useState(false);
+
+  function handleLockUnlock(): void {
+    if(props.locked){
+      setOpenDialog(true)
+    } else {
+      props.setLocked(true)
+    }
+  }
+
+  return (
+    <RestrictedAccess explicit={false}>
+      <Tooltip title={props.locked ? "Unlock for editing" : "Lock editing"}>
+        <Fab sx={{position: 'absolute', top: 10, right: 10}} color={props.locked ? "success" : "error"} aria-label="unlock" onClick={handleLockUnlock}>
+          {props.locked ? <LockOutlineIcon/> : <LockOpenIcon/>}
+        </Fab>
+      </Tooltip>
+      <Dialog   
+        open={openDialog}
+        slots={{
+          transition: TransitionUp,
+        }}
+        keepMounted
+        onClose={()=>setOpenDialog(false)}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{`Hey ${props.userFirstName ?? "there"}, are you sure you want to edit this?!`}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Once this is edited it will become visible to the public.
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-slide-description">
+            Are you sure you want to continue?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={()=>{setOpenDialog(false); props.setLocked(true)}}>Cancel</Button>
+          <Button onClick={()=>{setOpenDialog(false); props.setLocked(false)}}>Continue</Button>
+        </DialogActions>
+      </Dialog>
+    </RestrictedAccess>
+  )
+}
---
+++ app/components/Tansitions.tsx
@@ -0,0 +1,13 @@
+import { Slide } from "@mui/material";
+import { TransitionProps } from "@mui/material/transitions";
+import { forwardRef } from "react";
+
+export const TransitionUp = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+  ) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  }
+)
