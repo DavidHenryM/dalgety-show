c1b3f19b1aa044276d160a0f903ff6a6543f9df3
David Moorhouse <david@locusinnovations.com.au>
2026-01-26 11:22:07 +1100

    ui for admin table

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-import { Button, Container, Divider, Grid, Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from "@mui/material";
+import { Button, Container, Divider, Grid, Link, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
 import { drawerWidth, footerHeight } from "../settings"
 import { Background } from "../components/Background";
 import sheepJudging from '../images/gallery/Sheep_6.jpg'
 import { BulletList } from "../components/BulletList";
+import EditLock from "../components/EditLock";
+import RestrictedAccess from "../components/Restricted";
+import { SponsorshipPackagesTable } from "../components/SponsorshipPackagesTable";
+import { authClient } from "../lib/auth-client";
 
 export default function Sponsors(){
+  const { data } = authClient.useSession()
+  const [locked, setLocked] = useState<boolean>(true)
 
   return (
     <>
@@ -28,49 +35,62 @@ export default function Sponsors(){
           }, 
           mt: "25px", 
           mb: footerHeight,
-          justifyContent: "center"
+          justifyContent: "center",
+          position: "relative"
         }}
       >
-        <Grid size={6}>
+        <EditLock locked={locked} setLocked={setLocked} userFirstName={data?.user.name}/>
+        {!locked ? (
+          <RestrictedAccess explicit={true}>
+            <Grid size={12} spacing={2}>
+              <SponsorshipPackagesTable title="Sponsorship Packages" />
+            </Grid>
+          </RestrictedAccess>
+        ) : null}
+        <Grid size={{ xs: 12, md: 6 }}>
           <Paper sx={{p:2, backgroundColor: "secondary.main"}}>
-            <Typography component={"p"}>
-              The Dalgety Show is an annual community event celebrating the best of local
-              agriculture, arts, and community spirit. Each year, we bring together families,
-              friends, and supporters from across the region, and we could not do it without our
-              incredible sponsors, volunteers, and community supporters. Your generosity
-              ensures this valued community tradition continues to thrive, and we are sincerely
-              grateful for your support. 
-            </Typography>
-            <Typography component={"p"}>
-              With over 3000 people through the gate, entrants and visitors travelling from afar
+            <Stack spacing={1.5}>
+              <Typography component={"p"}>
+                The Dalgety Show is an annual community event celebrating the best of local
+                agriculture, arts, and community spirit. Each year, we bring together families,
+                friends, and supporters from across the region, and we could not do it without our
+                incredible sponsors, volunteers, and community supporters. Your generosity
+                ensures this valued community tradition continues to thrive, and we are sincerely
+                grateful for your support.
+              </Typography>
+              <Typography component={"p"}>
+                With over 3000 people through the gate, entrants and visitors travelling from afar
                 to enjoy the day this little country town puts on a great experience and cannot be
-                done without your generosity and support. 
-            </Typography>
-            <Typography component={"p"}>
-              This year there are more ways to support the Dalgety Show, select any, multiple or
+                done without your generosity and support.
+              </Typography>
+              <Typography component={"p"}>
+                This year there are more ways to support the Dalgety Show, select any, multiple or
                 all options set out below:
-            </Typography>
+              </Typography>
+            </Stack>
           </Paper>
         </Grid>
-        <Grid size={6}>
+        <Grid size={{ xs: 12, md: 6 }}>
           <Paper sx={{p:2, backgroundColor: "secondary.main"}}>
-            <Typography variant="h5">Sponsorship Package / Advertising</Typography>
-            <Typography variant="body1">
-              Contribute financially and receive recognition in the promotion of the show and listing in the show program & website. 
-              Option available to purchase additional advertising space for your business in the program.
-            </Typography>
-            <Divider/>
-            <Typography variant="h5">Major Raffle - Raffle Prize</Typography>
-            <Typography variant="body1" >
-              Donation of item/s, vouchers or business services as a raffle prize. 
-              Promotion will be via the dedicated raffle website & social media.
-            </Typography>
-            <Divider/>
-            <Typography variant="h5">Platypus Plunge - Corporate</Typography>
-            <Typography variant="body1">
-              Select to be a corporate sponsor and get your very own platypus in the race.
-              Limit of 1 per business.
-            </Typography>
+            <Stack spacing={1.5}>
+              <Typography variant="h5">Sponsorship Package / Advertising</Typography>
+              <Typography variant="body1">
+                Contribute financially and receive recognition in the promotion of the show and listing in the show program & website.
+                Option available to purchase additional advertising space for your business in the program.
+              </Typography>
+              <Divider/>
+              <Typography variant="h5">Major Raffle - Raffle Prize</Typography>
+              <Typography variant="body1" >
+                Donation of item/s, vouchers or business services as a raffle prize.
+                Promotion will be via the dedicated raffle website & social media.
+              </Typography>
+              <Divider/>
+              <Typography variant="h5">Platypus Plunge - Corporate</Typography>
+              <Typography variant="body1">
+                Select to be a corporate sponsor and get your very own platypus in the race.
+                Limit of 1 per business.
+              </Typography>
+            </Stack>
           </Paper>
         </Grid>
         <Grid size={12} spacing={2}>
@@ -170,7 +190,7 @@ export default function Sponsors(){
               display: 'flex', 
               justifyContent: 'center', 
               alignItems: 'center', 
-              height: 200 // Optional: ensures vertical centering
+              height: 120
             }}>
               <Link href="/sponsors/sponsor">
                 <Button variant="outlined" sx={{p:2}}>Become a Sponsor</Button>
